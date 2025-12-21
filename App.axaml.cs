3424480cc5583a708b9595334ae3ca83a228ee18
Peter van de Pas <peter.vdpas@protonmail.com>
2025-12-21 22:58:54 +0100

    show icon in windows

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,6 +1,9 @@
+using System;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
 using JingleBox2.UI;
 
 namespace JingleBox2;
@@ -18,7 +21,10 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow();
+            desktop.MainWindow = new MainWindow
+            {
+                Icon = new WindowIcon(AssetLoader.Open(new Uri("avares://JingleBox2/Assets/icon.ico")))
+            };
         }
 
         base.OnFrameworkInitializationCompleted();
